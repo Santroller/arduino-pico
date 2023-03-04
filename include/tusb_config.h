2a46affc65957dbea4c876d37e2abb2595474b65
Sanjay Govind <sanjay.govind9@gmail.com>
2023-03-04 22:11:39 +1300

    actually build updated packages

--- include/tusb_config.h
+++ include/tusb_config.h
@@ -27,7 +27,7 @@
 #define _TUSB_CONFIG_H_
 
 #ifdef __cplusplus
- extern "C" {
+extern "C" {
 #endif
 
 //--------------------------------------------------------------------
@@ -35,30 +35,20 @@
 //--------------------------------------------------------------------
 
 #ifndef CFG_TUSB_MCU
- #define CFG_TUSB_MCU             OPT_MCU_RP2040
+#define CFG_TUSB_MCU OPT_MCU_RP2040
 #endif
 
-#define CFG_TUSB_RHPORT0_MODE     OPT_MODE_DEVICE
-#define CFG_TUSB_OS               OPT_OS_PICO
+#define CFG_TUSB_RHPORT0_MODE OPT_MODE_DEVICE
+#define CFG_TUSB_RHPORT1_MODE OPT_MODE_HOST
+#define CFG_TUSB_OS OPT_OS_PICO
 
+#ifndef BOARD_TUH_MAX_SPEED
+#define BOARD_TUH_MAX_SPEED OPT_MODE_LOW_SPEED
+#endif
+#define CFG_TUH_RPI_PIO_USB 1
 // CFG_TUSB_DEBUG is defined by compiler in DEBUG build
 #ifndef CFG_TUSB_DEBUG
-#define CFG_TUSB_DEBUG           0
-#endif
-
-/* USB DMA on some MCUs can only access a specific SRAM region with restriction on alignment.
- * Tinyusb use follows macros to declare transferring memory so that they can be put
- * into those specific section.
- * e.g
- * - CFG_TUSB_MEM SECTION : __attribute__ (( section(".usb_ram") ))
- * - CFG_TUSB_MEM_ALIGN   : __attribute__ ((aligned(4)))
- */
-#ifndef CFG_TUSB_MEM_SECTION
-#define CFG_TUSB_MEM_SECTION
-#endif
-
-#ifndef CFG_TUSB_MEM_ALIGN
-#define CFG_TUSB_MEM_ALIGN          __attribute__ ((aligned(4)))
+#define CFG_TUSB_DEBUG 0
 #endif
 
 //--------------------------------------------------------------------
@@ -66,26 +56,14 @@
 //--------------------------------------------------------------------
 
 #ifndef CFG_TUD_ENDPOINT0_SIZE
-#define CFG_TUD_ENDPOINT0_SIZE    64
+#define CFG_TUD_ENDPOINT0_SIZE 64
 #endif
 
-//------------- CLASS -------------//
-#define CFG_TUD_HID              (2)
-#define CFG_TUD_CDC              (1)
-#define CFG_TUD_MSC              (1)
-#define CFG_TUD_MIDI             (0)
-#define CFG_TUD_VENDOR           (0)
-
-#define CFG_TUD_CDC_RX_BUFSIZE  (256)
-#define CFG_TUD_CDC_TX_BUFSIZE  (256)
-
-#define CFG_TUD_MSC_EP_BUFSIZE  (64)
-
-// HID buffer size Should be sufficient to hold ID (if any) + Data
-#define CFG_TUD_HID_EP_BUFSIZE  (64)
+#define CFG_TUH_ENDPOINT_MAX 8
+#define CFG_TUD_ENDPOINT_MAX 8
 
 #ifdef __cplusplus
- }
+}
 #endif
 
 #endif /* _TUSB_CONFIG_H_ */
